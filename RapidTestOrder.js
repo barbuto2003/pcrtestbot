f09d1c56a3fdaba8bc1df3a4dc8168bb762200a1
barbuto2003 <barb6470@mylaurier.ca>
2024-02-08 21:01:52 -0500

    change rapid test to coffee

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -4,8 +4,8 @@ class RapidTestOrder {
       WELCOMING: () => {
         let aReturn = [];
         this.stateCur = this.OrderState.RESERVING;
-        aReturn.push("Welcome to Rich's Rapid Test.");
-        aReturn.push("Would you like to reserve a rapid test kit?");
+        aReturn.push("Welcome to Emma's Coffee House!");
+        aReturn.push("What size coffee would you like?");
         return aReturn;
       },
       RESERVING: (sInput) => {
